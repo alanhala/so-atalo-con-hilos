536910f985ce6c6326015ff01c440d9382020b44
Ezequiel Haydossian <ezeqhay@hotmail.com>
2016-04-28 20:25:48 -0300

    creo thread para manejar comunicacion y envio mensajes y recibo

--- SWAP/main.c
+++ SWAP/main.c
@@ -32,23 +32,51 @@
 #define LISTENPORT "8000"
 #define BACKLOG 10
 
-
 void UMC_connection();
+void *UMC_connection_handler(int umc_socket_descriptor);
 
 int main(int argc, char *argv[]) {
 
 	UMC_connection();
-
+	while (1) {
+		//TODO preguntar a ayudante que hago para que no finalice si los threads estan abiertos
+		sleep(10);
+	}
 	return 0;
 }
 
 void UMC_connection() {
-	int server_socket_descriptor = create_server_socket_descriptor(LISTENPORT, 10);
-	while (1) {
-		int umc_socket_descriptor = accept_connection(
-				server_socket_descriptor);
-		printf("se conecto umc");
-		fflush(stdout);
+	//Solo se va a conectar un proceso UMC
+
+	int server_socket_descriptor = create_server_socket_descriptor(LISTENPORT,
+			10);
+	int umc_socket_descriptor = accept_connection(server_socket_descriptor);
+
+	pthread_t thread;
+	int thread_result = pthread_create(&thread, NULL, &UMC_connection_handler,
+			umc_socket_descriptor);
+	//TODO preguntar a ayudante que pasa aca con los argumentos
+
+	if (thread_result) {
+		// TODO LOGUEAR ERROR
+		// TODO Analizar el tratamiento que desea darse
+		printf("Error - pthread_create() return code: %d\n", thread_result);
+		exit(1);
 	}
+	printf("Hilo creado \n"); //TODO BORRAR LINEA
+
 }
 
+void *UMC_connection_handler(int umc_socket_descriptor) {
+	printf("se conecto umc\n");
+	fflush(stdout);
+	char message[] = "bienvenido umc\n";
+	while (1) {
+		char recvBuffer[19];
+		recv(umc_socket_descriptor, recvBuffer, 19, 0);
+		printf(recvBuffer);
+		fflush(stdout);
+		send(umc_socket_descriptor, message, 15, 0);
+		sleep(1);
+	}
+}
--- UMC/main.c
+++ UMC/main.c
@@ -32,14 +32,18 @@ void connect_to_SWAP();
 
 int main(int argc, char **argv) {
 
-
 	connect_to_SWAP();
+
+	while (1) {
+		sleep(10);
+	}
 
 	return 0;
 }
 
 void kernel_and_cpu_connections() {
-	int server_socket_descriptor = create_server_socket_descriptor(LISTENPORT, BACKLOG);
+	int server_socket_descriptor = create_server_socket_descriptor(LISTENPORT,
+			BACKLOG);
 	while (1) {
 		int client_socket_descriptor = accept_connection(
 				server_socket_descriptor);
@@ -47,10 +51,22 @@ void kernel_and_cpu_connections() {
 }
 
 void connect_to_SWAP() {
-	int swap_socket_descriptor = create_client_socket_descriptor(SWAPIP, SWAPPORT);
-	printf("se pudo conectar al swap");
+	int swap_socket_descriptor = create_client_socket_descriptor(SWAPIP,
+			SWAPPORT);
+	printf("Conectado al Swap");
 	fflush(stdout);
-	while(1){
+	char message[] = "hola swap, soy umc\n";
+
+	while (1) { //TODO REVISAR por que hace print de lo que envia y de lo que recibe. puede que ser que sea por el socket?
+		//char header[1];
+		char recvBuffer[15];
+		send(swap_socket_descriptor, "hola swap, soy umc\n", 19, 0);
+		//recv(swap_socket_descriptor, header, 2, 0);
+		//char recvBuffer[header];
+		recv(swap_socket_descriptor, recvBuffer, 15, 0);
+
+		printf(recvBuffer);
+		fflush(stdout);
 		sleep(1);
 	}
 
