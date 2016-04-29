24c07fa70dfa1c6bf0ee7d0c07ea459c2207c926
Ezequiel Haydossian <ezeqhay@hotmail.com>
2016-04-29 01:53:57 -0300

    comunico swap umc y cpu todos juntos

--- CPU/cpu_main.c
+++ CPU/cpu_main.c
@@ -25,21 +25,55 @@
 #define UMCPORT "21000"
 #define UMCIP "localhost"
 
-
 void connect_to_UMC();
 void connect_to_Kernel();
+void *connect_to_UMC_thread();
 
-int main(int argc, char **argv) {
-	void connect_to_UMC();
-	void connect_to_Kernel();
+int
+
+main(int argc, char **argv) {
+	connect_to_UMC();
+	connect_to_Kernel();
+
+	while (1) { // para que no se cierren los threads
+		sleep(10);
+	}
+
 	return 0;
 }
 
-void connect_to_UMC(){
+void connect_to_UMC() {
+	pthread_t thread;
+	int thread_result = pthread_create(&thread, NULL, &connect_to_UMC_thread,
+			NULL);
+
+	if (thread_result) {
+		// TODO LOGUEAR ERROR
+		// TODO Analizar el tratamiento que desea darse
+		printf("Error - pthread_create() return code: %d\n", thread_result);
+		exit(1);
+	}
 
 }
 
+void *connect_to_UMC_thread() {
+	int umc_socket_descriptor = create_client_socket_descriptor(UMCIP,
+			UMCPORT);
 
-void connect_to_Kernel(){
+	while (1) { //TODO REVISAR por que hace print de lo que envia y de lo que recibe. puede que ser que sea por el socket?
+			//char header[1];
+			char recvBuffer[15];
+			send(umc_socket_descriptor, "holaa umc, soy cpu\n", 19, 0);
+			//recv(swap_socket_descriptor, header, 2, 0);
+			//char recvBuffer[header];
+			recv(umc_socket_descriptor, recvBuffer, 15, 0);
+
+			printf(recvBuffer);
+			fflush(stdout);
+			sleep(1);
+		}
+}
+
+void connect_to_Kernel() {
 
 }
--- UMC/main.c
+++ UMC/main.c
@@ -27,12 +27,16 @@
 #define SWAPIP "localhost" // TODO es necesario?
 #define BACKLOG 10
 
+void *kernel_and_cpu_connection_thread();
 void kernel_and_cpu_connections();
 void connect_to_SWAP();
+void *connect_to_SWAP_thread();
+void *kernel_and_cpu_connection_handler(int client_socket_descriptor);
 
 int main(int argc, char **argv) {
 
 	connect_to_SWAP();
+	kernel_and_cpu_connections();
 
 	while (1) {
 		sleep(10);
@@ -42,17 +46,56 @@ int main(int argc, char **argv) {
 }
 
 void kernel_and_cpu_connections() {
-	int server_socket_descriptor = create_server_socket_descriptor(LISTENPORT,
-			BACKLOG);
-	while (1) {
-		int client_socket_descriptor = accept_connection(
-				server_socket_descriptor);
+	pthread_t thread;
+	int thread_result = pthread_create(&thread, NULL,
+			&kernel_and_cpu_connection_thread, NULL);
+
+	if (thread_result) {
+		// TODO LOGUEAR ERROR
+		// TODO Analizar el tratamiento que desea darse
+		printf("Error - pthread_create() return code: %d\n", thread_result);
+		exit(1);
 	}
 }
 
-void connect_to_SWAP() {
+void *kernel_and_cpu_connection_thread() {
+	int server_socket_descriptor = create_server_socket_descriptor(LISTENPORT,
+	BACKLOG);
+
+	while (1) {
+		int client_socket_descriptor = accept_connection(
+				server_socket_descriptor);
+
+		pthread_t thread;
+		int thread_result = pthread_create(&thread, NULL,
+				&kernel_and_cpu_connection_handler, client_socket_descriptor);
+		if (thread_result) {
+			// TODO LOGUEAR ERROR
+			// TODO Analizar el tratamiento que desea darse
+			printf("Error - pthread_create() return code: %d\n", thread_result);
+			exit(1);
+		}
+		printf("Hilo creado \n"); //TODO BORRAR LINEA
+
+	}
+}
+
+void connect_to_SWAP(){
+	pthread_t thread;
+	int thread_result = pthread_create(&thread, NULL,
+			&connect_to_SWAP_thread, NULL);
+
+	if (thread_result) {
+		// TODO LOGUEAR ERROR
+		// TODO Analizar el tratamiento que desea darse
+		printf("Error - pthread_create() return code: %d\n", thread_result);
+		exit(1);
+	}
+}
+
+void *connect_to_SWAP_thread() {
 	int swap_socket_descriptor = create_client_socket_descriptor(SWAPIP,
-			SWAPPORT);
+	SWAPPORT);
 	printf("Conectado al Swap");
 	fflush(stdout);
 	char message[] = "hola swap, soy umc\n";
@@ -70,4 +113,17 @@ void connect_to_SWAP() {
 		sleep(1);
 	}
 
+}
+
+void *kernel_and_cpu_connection_handler(int client_socket_descriptor) {
+	//TODO aca deberia ir el handshake para ver si lo trato como kernel o como cpu
+	char message[] = "bienvenido \n";
+	while (1) {
+		char recvBuffer[19];
+		recv(client_socket_descriptor, recvBuffer, 19, 0);
+		printf(recvBuffer);
+		fflush(stdout);
+		send(client_socket_descriptor, message, 12, 0);
+		sleep(1);
+	}
 }
