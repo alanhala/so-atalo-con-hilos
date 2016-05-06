460a058fdc987ac0094224527d77b846af42f49b
Ezequiel Haydossian <ezeqhay@hotmail.com>
2016-05-06 15:30:04 -0300

    soy muy crack

--- umc_playground/umc_play_main.c
+++ umc_playground/umc_play_main.c
@@ -33,18 +33,46 @@ t_list* tablas_de_paginas;
 sem_t mut_tabla_de_paginas;
 
 int main(int argc, char **argv) {
-	int carga = cargar_archivo_configuracion();
+	//zona de test
 	int init = inicializar_estructuras();
+	printf("inicie las estructuras \n");
+	fflush(stdout);
+	cargar_nuevo_programa(100, 200);
+	printf("creo un programa con pid 100 \n");
+	fflush(stdout);
+	cargar_nuevo_programa(200, 200);
+	printf("creo un programa con pid 200 \n");
+	fflush(stdout);
+
+	t_tablas_de_paginas* tabla= dame_tabla_de_paginas_de_pid(100);
+	//TODO ANALIZAR LO QUE DEVUELVE CUANDO NO ENCUENTRA LA PAGINA PORQUE NO AVANZO CON EL PROGRAMA
+	printf("busco la tabla de paginas de PID:%d\n", tabla->pid);
+	fflush(stdout);
+
+	// fin zona de test
+
+	while(1){
+
+	}
+
+
+	//int carga = cargar_archivo_configuracion();
+	//int init = inicializar_estructuras();
 	return 0;
 }
 
 int inicializar_estructuras(){
+	inicializar_semaforos();
 	TAMANIO_MEMORIA_PRINCIPAL = TAMANIO_MARCO * CANTIDAD_MARCOS;
 	crear_memoria_principal();
 	tablas_de_paginas= list_create();
 	return 0;
 }
 
+void inicializar_semaforos(){
+	sem_init(&mut_tabla_de_paginas, 0, 1);
+
+}
 int cargar_archivo_configuracion(){
 	//TODO cargar archivo de configuracion y a partir de eso setear los valores
 	TAMANIO_MARCO=50;
@@ -64,28 +92,42 @@ void liberar_memoria_principal()
 }
 
 
-void cargar_nuevo_programa(int pid, int paginas_requeridas_del_proceso, t_list* tablasDePaginas){
-
-	crear_tabla_de_pagina_de_un_proceso(pid,  paginas_requeridas_del_proceso,  tablasDePaginas );
+void cargar_nuevo_programa(int pid, int paginas_requeridas_del_proceso){
+	//TODO validar si hay espacio en memoria principal
+	crear_tabla_de_pagina_de_un_proceso(pid,  paginas_requeridas_del_proceso );
 
 }
 
-entrada_tabla_de_paginas* inicializar_paginas(int paginas_requeridas){
-	entrada_tabla_de_paginas* entradas = malloc(sizeof(entrada_tabla_de_paginas)* paginas_requeridas);
+t_entrada_tabla_de_paginas* inicializar_paginas(int paginas_requeridas_del_proceso){
+	t_entrada_tabla_de_paginas* entradas = malloc(sizeof(t_entrada_tabla_de_paginas)* paginas_requeridas_del_proceso);
 	return entradas;
 }
 
 
-void crear_tabla_de_pagina_de_un_proceso(int pid, int paginas_requeridas_del_proceso, t_list* tablas_de_pags ){
+void crear_tabla_de_pagina_de_un_proceso(int pid, int paginas_requeridas_del_proceso){
 
-	entrada_tabla_de_paginas* entradas = inicializar_paginas(paginas_requeridas_del_proceso);
-	tabla_de_paginas* nueva_tabla = malloc(sizeof(tabla_de_paginas));
+	t_entrada_tabla_de_paginas* entradas = inicializar_paginas(paginas_requeridas_del_proceso);
+	t_tablas_de_paginas* nueva_tabla = malloc(sizeof(t_tablas_de_paginas));
 	nueva_tabla->pid=pid;
 	nueva_tabla->paginas_totales=paginas_requeridas_del_proceso;
 	nueva_tabla->entradas=entradas;
 	sem_wait(&mut_tabla_de_paginas);
 	list_add(tablas_de_paginas, nueva_tabla);
 	sem_post(&mut_tabla_de_paginas);
+}
+
+
+
+t_tablas_de_paginas* dame_tabla_de_paginas_de_pid(int pid_buscado){
+	sem_wait(&mut_tabla_de_paginas);
+
+	bool pid_iguales(t_tablas_de_paginas* tabla) {
+						return (tabla->pid == pid_buscado);}
+
+	t_tablas_de_paginas* tabla = list_find(tablas_de_paginas, (void*) pid_iguales);
+	sem_post(&mut_tabla_de_paginas);
+	return tabla;
+
 }
 
 
--- umc_playground/umc_play_main.h
+++ umc_playground/umc_play_main.h
@@ -1,18 +1,20 @@
 typedef struct e_t_p{
 	int frame;
-} entrada_tabla_de_paginas;
+} t_entrada_tabla_de_paginas;
 
 typedef struct t_p {
 	int pid;
 	int paginas_totales;
-	entrada_tabla_de_paginas* entradas;
+	t_entrada_tabla_de_paginas* entradas;
 
-} tabla_de_paginas;
+} t_tablas_de_paginas;
 
 void crear_memoria_principal();
 int cargar_archivo_configuracion();
 void liberar_memoria_principal();
 int inicializar_estructuras();
-void cargar_nuevo_programa(int pid, int paginas_requeridas_del_proceso, t_list* tablasDePaginas);
-void crear_tabla_de_pagina_de_un_proceso(int pid, int paginas_requeridas_del_proceso, t_list* tablas_de_pags );
-entrada_tabla_de_paginas* inicializar_paginas(int paginas_requeridas);
+void cargar_nuevo_programa(int pid, int paginas_requeridas_del_proceso);
+void crear_tabla_de_pagina_de_un_proceso(int pid, int paginas_requeridas_del_proceso);
+t_entrada_tabla_de_paginas* inicializar_paginas(int paginas_requeridas);
+t_tablas_de_paginas* dame_tabla_de_paginas_de_pid(int pid_buscado);
+void inicializar_semaforos();
