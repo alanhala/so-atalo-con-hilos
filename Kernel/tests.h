d0dc38b412bc625a239111b4a2d4cf63d9b414b1
Matias <matias.fra@gmail.com>
2016-05-09 00:33:09 -0300

    funcion que calcula el total de paginas necesarias para el codigo y su test

---
+++ Kernel/tests.h
@@ -0,0 +1,16 @@
+/*
+ * tests.h
+ *
+ *  Created on: 9/5/2016
+ *      Author: utnso
+ */
+
+#ifndef TESTS_H_
+#define TESTS_H_
+
+
+
+#endif /* TESTS_H_ */
+
+int correrTest();
+void obtengo_paginas_para_programa();
