55234ae6e1d88977380ff5fe91a40c09ee2aa482
Alan Halatian <alanhalatian@gmail.com>
2016-05-27 14:10:17 -0300

    arreglo algoritmo de busqueda de espacio en swap, agrego leer swap y escribir en swap

--- SWAP/main.c
+++ SWAP/main.c
@@ -28,59 +28,96 @@
 #include <signal.h>
 #include <pthread.h>
 #include "socket.h"
+#include "swap.h"
+#include "protocolo_swap.h"
 
 #define LISTENPORT "8000"
 #define BACKLOG 10
 
-void UMC_connection();
-void *UMC_connection_handler(int umc_socket_descriptor);
+void UMC_connection(t_swap* swap);
 
 int main(int argc, char *argv[]) {
-		if (strcmp(argv[1], "-test") == 0){
-			correr_protocolo_test();
-			return 0;
-		}
-
-	UMC_connection();
-	while (1) {
-		//TODO preguntar a ayudante que hago para que no finalice si los threads estan abiertos
-		sleep(10);
-	}
+//		if (strcmp(argv[1], "-test") == 0){
+//			correr_protocolo_test();
+//			return 0;
+//		}
+	t_swap* swap = create_swap();
+	UMC_connection(swap);
 	return 0;
 }
 
-void UMC_connection() {
+void UMC_connection(t_swap* swap) {
 	//Solo se va a conectar un proceso UMC
 
-	int server_socket_descriptor = create_server_socket_descriptor(LISTENPORT,
-			10);
+	int server_socket_descriptor = create_server_socket_descriptor("192.168.0.102",
+			LISTENPORT,	10);
 	int umc_socket_descriptor = accept_connection(server_socket_descriptor);
 
-	pthread_t thread;
-	int thread_result = pthread_create(&thread, NULL, &UMC_connection_handler,
-			umc_socket_descriptor);
-	//TODO preguntar a ayudante que pasa aca con los argumentos
 
-	if (thread_result) {
-		// TODO LOGUEAR ERROR
-		// TODO Analizar el tratamiento que desea darse
-		printf("Error - pthread_create() return code: %d\n", thread_result);
-		exit(1);
-	}
-	printf("Hilo creado \n"); //TODO BORRAR LINEA
-
-}
-
-void *UMC_connection_handler(int umc_socket_descriptor) {
-	printf("se conecto umc\n");
-	fflush(stdout);
-	char message[] = "bienvenido umc\n";
 	while (1) {
-		char recvBuffer[19];
-		recv(umc_socket_descriptor, recvBuffer, 19, 0);
-		printf(recvBuffer);
-		fflush(stdout);
-		send(umc_socket_descriptor, message, 15, 0);
-		sleep(1);
+		char code[1];
+		recv(umc_socket_descriptor, code, 1, 0);
+		if (code[0] == '1') {
+
+			char recv_buffer[200];
+			int bytesrecv =recv(umc_socket_descriptor, recv_buffer, 200, 0);
+
+
+			t_iniciar_programa_en_swap * carga = malloc(sizeof(t_iniciar_programa_en_swap));
+			carga = (t_iniciar_programa_en_swap*)deserealizar_mensaje(20, recv_buffer);
+
+			int resultado_carga =initialize_program(swap, carga->pid, carga->paginas_necesarias, carga->codigo_programa);
+
+			t_respuesta_iniciar_programa_en_swap *respuesta_inicio_programa = malloc(sizeof(t_respuesta_iniciar_programa_en_swap));
+			memset(respuesta_inicio_programa,0,sizeof(t_respuesta_iniciar_programa_en_swap));
+			respuesta_inicio_programa->cargado_correctamente = resultado_carga;
+			t_stream *buffer = serializar_mensaje(20,respuesta_inicio_programa);
+
+			int bytes= send(umc_socket_descriptor, buffer->datos, 50, 0);
+
+
+		}
+		if (code[0] == '2') {
+			char recv_buffer_lectura[50];
+			int bytesrecv_lectura =recv(umc_socket_descriptor, recv_buffer_lectura, 50, 0);
+
+
+			t_leer_pagina_swap * lectura = malloc(sizeof(t_leer_pagina_swap));
+			lectura = (t_leer_pagina_swap*)deserealizar_mensaje(22, recv_buffer_lectura);
+
+			char *contenido = read_page(swap, lectura->pid, lectura->pagina);
+			t_respuesta_leer_pagina_swap *respuesta_bytes = malloc(sizeof(t_respuesta_leer_pagina_swap));
+			memset(respuesta_bytes,0,sizeof(t_respuesta_leer_pagina_swap));
+			respuesta_bytes->datos = contenido;
+			t_stream *buffer = serializar_mensaje(22,respuesta_bytes);
+
+			int bytes2= send(umc_socket_descriptor, buffer->datos, 50, 0);
+
+				}
+		if (code[0] == '3') {
+			char recv_buffer_escritura[50];
+			int bytesrecv =recv(umc_socket_descriptor, recv_buffer_escritura, 50, 0);
+
+
+			t_escribir_pagina_swap * carga = malloc(sizeof(t_escribir_pagina_swap));
+			carga = (t_escribir_pagina_swap*)deserealizar_mensaje(26, recv_buffer_escritura);
+
+			int res = write_page(swap, carga->pid, carga->pagina, carga->datos);
+
+			t_respuesta_escribir_pagina_swap *respuesta = malloc(sizeof(t_respuesta_escribir_pagina_swap));
+			memset(respuesta,0,sizeof(t_respuesta_escribir_pagina_swap));
+			respuesta->escritura_correcta = res;
+			t_stream *buffer_escritura = serializar_mensaje(26,respuesta);
+
+			int bytes= send(umc_socket_descriptor, buffer_escritura->datos, 50, 0);
+
+
+		}
+
+		if (code[0] == '4') {
+			break;
+		}
+
 	}
 }
+
---
+++ SWAP/spec/swap_spec.h
@@ -0,0 +1,12 @@
+#ifndef SWAP_SPEC_H
+#define SWAP_SPEC_H
+
+	#include "../swap.h"
+	#include "CUnit/Basic.h"
+
+	int test();
+	void loads_configuration_file();
+	void creates_file();
+	void initializes_the_bitmap();
+
+#endif
