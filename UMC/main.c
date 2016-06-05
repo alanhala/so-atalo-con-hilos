5b4acbf749e830226fb333beb7879b3a40919d8e
Matias <matias.fra@gmail.com>
2016-06-05 16:09:50 -0300

    fix de la mano de TONY

--- UMC/main.c
+++ UMC/main.c
@@ -192,7 +192,7 @@ void manejo_de_solicitudes(int socket_descriptor) {
 	if(handshake == 2) //KERNEL
 	{
 		cargar_nuevo_programa(1, 50, "begin\nvariables c, d\nc=1234\nd=4321\nend\0");
-		//cargar_nuevo_programa(2, 50, "cargo un programa");
+		cargar_nuevo_programa(2, 50, "cargo un programa");
 	}
 
 	while (1) {
