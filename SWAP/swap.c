7d307937fc33109725e914915de286f921ef1f7f
Alan Halatian <alanhalatian@gmail.com>
2016-07-06 23:55:02 -0300

    aca esta la papa

--- SWAP/swap.c
+++ SWAP/swap.c
@@ -118,7 +118,7 @@ void write_swap_file(t_swap* self, int first_page_location, unsigned int pages_a
 void write_swap_page_file(t_swap* self, int first_page_location, unsigned int pages_amount, char * page_data) {
 	int i;
 	if (fseek(self->file, first_page_location, SEEK_SET) == 0) {
-		fwrite(page_data, get_page_size(), 1, self->file);
+		fwrite(page_data, self->page_size, 1, self->file);
 		int not_written_bytes = pages_amount * self->page_size - get_page_size();
 		if (not_written_bytes > 0) {
 			for (i = 0; i < not_written_bytes; i++)
